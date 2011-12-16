56653093ade4c4d244ec9f500bd722d430764958
Michal Cichra <michal@o2h.cz>
2011-12-16 07:47:04 +0100

    fixed /etc/default/openvpn file

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,10 @@
 class openvpn {
+
     package {
         "openvpn":
             ensure => installed;
     }
+
     service {
         "openvpn":
             ensure     => running,
@@ -10,22 +12,24 @@ class openvpn {
             hasstatus  => true,
             require    => Exec["/etc/default/openvpn concatenation"];
     }
+
     file {
         "/etc/openvpn":
             ensure  => directory,
             require => Package["openvpn"];
     }
+
     file {
         "/etc/openvpn/keys":
             ensure  => directory,
             require => File["/etc/openvpn"];
     }
-    common::concatfilepart {
-        "00-etc-default-openvpn header":
+
+    file {
+      "/etc/default/openvpn":
             ensure  => present,
             content => template("openvpn/etc-default-openvpn.erb"),
-            notify  => Service["openvpn"],
-            file    => "/etc/default/openvpn";
+            notify  => Service["openvpn"];
     }
 
     user {
