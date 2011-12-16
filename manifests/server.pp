82ff9a0329c497cd2bebc9b4966a39a532cc03c2
Michal Cichra <michal@o2h.cz>
2011-12-16 07:51:44 +0100

    fixed template name

--- manifests/server.pp
+++ manifests/server.pp
@@ -2,7 +2,7 @@ class openvpn::server {
   include openvpn
 
   file { "/etc/openvpn/server.conf":
-    content => template("openvpn-server/server.conf.erb"),
+    content => template("openvpn/server.conf.erb"),
     ensure => present,
     owner => openvpn,
     group => openvpn,
