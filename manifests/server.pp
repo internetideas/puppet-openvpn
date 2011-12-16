9bb6b16d9ccb4a0f94fa71223b4012498c9c98bc
Michal Cichra <michal@o2h.cz>
2011-12-16 09:22:10 +0100

    generate dh key

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,12 +1,34 @@
 class openvpn::server {
   include openvpn
 
-  file { "/etc/openvpn/server.conf":
-    content => template("openvpn/server.conf.erb"),
-    ensure => present,
-    owner => openvpn,
-    group => openvpn,
-    require => File["/etc/openvpn"];
+  file {
+    "/etc/openvpn/server.conf":
+      content => template("openvpn/server.conf.erb"),
+      ensure => present,
+      owner => openvpn,
+      group => openvpn,
+      require => [ File["/etc/openvpn"], File["/etc/openvpn/keys/dh1024.pem"] ];
   }
 
+  package {
+    "openssl":
+      ensure => installed;
+  }
+
+  exec {
+    "generate dh":
+      command  => "openssl dhparam -out dh1024.pem 1024",
+      cwd      => "/etc/openvpn/keys/",
+      creates  => "/etc/openvpn/keys/dh1024.pem",
+      provider => "shell",
+      require  => [ File["/etc/openvpn/keys"], Package['openssl'] ];
+  }
+
+  file {
+    "/etc/openvpn/keys/dh1024.pem":
+      content => Exec['generate dh'],
+      ensure => present,
+      owner => openvpn,
+      group => openvpn
+  }
 }
